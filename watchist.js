adc2249f4c0d30a5825a8f471cfae2eb3c6a7d93
Misir Jafarov <misir.ceferov@gmail.com>
2020-04-21 22:12:00 +0400

    Update watchist.js

--- watchist.js
+++ watchist.js
@@ -5,21 +5,21 @@
  * @license MIT
  ********************************************************/
 !function(w, l){
-    w.Watchist = w.Watchist || {};
-    w.Watchist.poll = function(){
-        setTimeout(function() {
-            fetch(w.Watchist.host || 'http://localhost:2588').then(function(response) {
-                w.Watchist.poll();
-                response.json().then(function(json) {
-                    if ('update' in json && json.update) {
-                        if (typeof w.Watchist.reload == 'function') w.Watchist.reload();
-                        else l.reload();
-                    }
-                });
-            }).catch(function() {
-                w.Watchist.poll();
-            });
-        }, 1000);
+  w.Watchist = w.Watchist || {};
+  w.Watchist.poll = function(){
+    setTimeout(function() {
+      fetch(w.Watchist.host || 'http://localhost:2588')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json) {
+          if (json && json.update) {
+            if (w.Watchist.reload) w.Watchist.reload();
+            else r.reload();
+          }
+        })
+        .finally(w.Watchist.poll);
+      }, 1000);
     };
     w.Watchist.poll();
 }(window, location);
